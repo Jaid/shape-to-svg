877eb846c8f71e2e358c026094478fe6a5013874
Jaid <6216144+Jaid@users.noreply.github.com>
2026-05-31 03:39:49 +0100

    Added input type definitions
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,41 @@
-const shapeToSvg = () => {
+type EmptyOptions = {}
+
+type ViewboxSquareOptions = {
+  size: number
+}
+
+type ViewboxRectOptions = {
+  height: number
+  width: number
+}
+
+type ViewboxFullOptions = {
+  viewbox: string
+}
+
+type ViewboxOptions = EmptyOptions | ViewboxFullOptions | ViewboxRectOptions | ViewboxSquareOptions
+
+type ColorStaticOptions = {
+  color: string
+}
+
+type ColorThemedOptions = {
+  color: {
+    onDark: string
+    onLight: string
+  }
+}
+
+type ColorOptions = ColorStaticOptions | ColorThemedOptions | EmptyOptions
+
+type Options = {
+  path: string
+} & ViewboxOptions & ColorOptions
+
+type InputOptions = Options | string
+
+const shapeToSvg = (input: InputOptions) => {
+  const options = typeof input === 'string' ? {path: input} : input
   return 'shape-to-svg' // TODO Implement actual functionality
 }
 
